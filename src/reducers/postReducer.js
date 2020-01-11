deb6f31a5e678da70ee6df2cd4e0230d869760bc
Rahul Bhaskar <rahulbhaskar@Rahuls-MacBook-Air.local>
2020-01-12 00:43:47 +0530

    MINOR CHNAGES

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,11 +1,11 @@
 const postReducer = (state = [], action) => {
     switch (action.type) {
     case 'ADD_POST':
-        return state.concat([action.data])
+        return state.concat([action.data]);
     case 'DELETE_POST':
-        return state.filter((post) => post.id !== action.id)
+        return state.filter((post) => post.id !== action.id);
     case 'EDIT_POST':
-        return state.map((post) => post.id === action.id ? { ...post, editing: !post.editing } : post)
+        return state.map((post) => post.id === action.id ? { ...post, editing: !post.editing } : post);
     case 'UPDATE':
         return state.map((post) => {
         if (post.id === action.id) {
@@ -16,7 +16,7 @@ const postReducer = (state = [], action) => {
         editing: !post.editing
         }
         } else return post;
-        })
+        });
     default:
         return state;
     }
